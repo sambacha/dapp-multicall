a2fb6e37dcc5bdb0d5f811daf76828055fd54249
sam bacha <sam@manifoldfinance.com>
2022-01-12 05:16:22 -0800

    build(package): dist

---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+exports.Multicall__factory = exports.IMulticall__factory = exports.DappInterfaceMulticall__factory = void 0;
+var DappInterfaceMulticall__factory_1 = require("./factories/DappInterfaceMulticall__factory");
+__createBinding(exports, DappInterfaceMulticall__factory_1, "DappInterfaceMulticall__factory");
+var IMulticall__factory_1 = require("./factories/IMulticall__factory");
+__createBinding(exports, IMulticall__factory_1, "IMulticall__factory");
+var Multicall__factory_1 = require("./factories/Multicall__factory");
+__createBinding(exports, Multicall__factory_1, "Multicall__factory");
